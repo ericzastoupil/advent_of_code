2b7e59baed32dfda9c4c8ab1b3cb957a974fe380
ericzastoupil <ericzastoupil@users.noreply.github.com>
2023-12-09 06:16:16 -0500

    Day8 part 2 complete

--- 2023/Day8/part2/solution.py
+++ 2023/Day8/part2/solution.py
@@ -1,26 +1,28 @@
 def get_input(lines):
     directions = lines[0].strip()
     temp_dict = {}
+    starting_nodes = []
 
     for line in lines[2:]:
         full_line = line.split(' = ')
         directions_tuple = (full_line[1].strip()[1:4], full_line[1].strip()[6:9])
         temp_dict[full_line[0]] = directions_tuple
+        if full_line[0][2] == 'A':
+            starting_nodes.append(full_line[0])
 
-    return directions, temp_dict
+    return starting_nodes, directions, temp_dict
 
-def play_game(directions, map_dict):
-
+def iterate_until_complete(node, directions):
     at_end = False
     dir_idx = 0
-    location = 'AAA'
+    location = node
     count = 0
 
     while not at_end:
         if dir_idx == len(directions):
             dir_idx = 0
             continue
-        if location == 'ZZZ':
+        if location[2] == 'Z':
             return count
         else:
             if directions[dir_idx] == 'L':
@@ -29,12 +31,22 @@ def play_game(directions, map_dict):
                 location = map_dict[location][1]
         
         count += 1
-        dir_idx += 1          
+        dir_idx += 1
+
+def play_game(nodes, directions):
+
+    freqs = []
+
+    for i in range(len(nodes)):
+        freqs.append(iterate_until_complete(nodes[i], directions))
+    
+    return freqs
 
 if __name__ == '__main__':
     file = open('../input.txt', 'r')
     lines = file.readlines()
 
-    directions, map_dict = get_input(lines)
+    starting_nodes, directions, map_dict = get_input(lines)
 
-    print(play_game(directions, map_dict))
+    frequencies = play_game(starting_nodes, directions)
+    print(frequencies) #lcm of each of these
