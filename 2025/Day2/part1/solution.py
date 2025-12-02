49703524306e9fc5582c3ea6a7e44f9ad1de2579
ericzastoupil <ericzastoupil@users.noreply.github.com>
2025-12-02 10:37:21 -0500

    Day 2 complete

---
+++ 2025/Day2/part1/solution.py
@@ -0,0 +1,37 @@
+def get_input(file_name):
+    file_list = []
+    with(open(file_name, 'r')) as file:
+        line = file.readline()
+        file_list = line.strip("\n").split(",")
+        
+    for i in range(0, len(file_list)):
+        file_list[i] = (int(file_list[i].split('-')[0]), int(file_list[i].split('-')[1]))
+
+    print(file_list)
+    return file_list
+
+def invalid_num(num):
+    str_num = str(num)
+
+    if str_num[:len(str_num) // 2] == str_num[len(str_num) // 2:]:
+        return num
+    
+    return 0
+
+def sum_invalids_in_range(ID_range):
+    sum_invalids_in_range = 0
+    for num in range(ID_range[0], ID_range[1] + 1):
+        sum_invalids_in_range += invalid_num(num)
+    return sum_invalids_in_range
+
+def play_game(file_list):
+
+    sum_all_invalids = 0
+
+    for range in file_list:
+        sum_all_invalids += sum_invalids_in_range(range)
+
+    return sum_all_invalids
+
+if __name__ == "__main__":
+    print("Solution: ", play_game(get_input("input.txt")))
